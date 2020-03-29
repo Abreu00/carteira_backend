2a5e7e2553f0e2d28776eb248c0f19e665f8ed20
Fabio de Abreu <fabiowabreu@gmail.com>
2020-03-29 17:06:33 -0300

    Initial commit, setup basic flask application

---
+++ app/routes.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify, request, Blueprint
+from .actives import actives_list
+
+actives = Blueprint('active', __name__)
+
+
+@actives.route('', methods=['GET'])
+def get_all():
+
+  return jsonify(actives_list)
